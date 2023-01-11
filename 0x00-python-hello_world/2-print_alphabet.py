7382e563bdbe5da19636bf723956fdc181843c8f
acidicForest0413 <5540@holbertonstudents.com>
2023-01-11 01:23:30 +0000

    all tasks

---
+++ 0x00-python-hello_world/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import random
+alphabet = random.randint(61, 122)
+print("{:d}(a;phabet))
---
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+message = "{:0=2d}"
+for num in range(100):
+	if(num <= 98):
+		print(message.format(num), end=", ")
+	else:
+		print(99)
