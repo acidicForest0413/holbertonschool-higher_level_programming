302c5353d86c9cd843c2806b26357aaa31d4245b
acidicForest0413 <5540@holbertonstudents.com>
2023-01-25 01:50:27 +0000

    all

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""doc check maybe"""
+
+
+def add_integer(a, b=98):
+    """comment test"""
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
