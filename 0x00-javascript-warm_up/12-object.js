bff7f4e2a53ce22518527cbd14019f33be65d187
acidicForest0413 <5540@holbertonstudents.com>
2023-04-12 16:36:34 +0000

    fixing semistandard

--- 0x00-javascript-warm_up/12-object.js
+++ 0x00-javascript-warm_up/12-object.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  
-  if (myObject.value === 12) {
-    myObject.value = 89;
-  }
-  
-  console.log(myObject);
-  
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+if (myObject.value === 12) {
+  myObject.value = 89;
+}
+
+console.log(myObject);
